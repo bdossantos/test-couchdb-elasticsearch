1bf7b81d21f1ff0e3e58b4ce372758b74ac7c351
Benjamin Dos Santos <benj.ds@gmail.com>
2012-07-17 11:22:44 +0200

    install CouchDB river

--- site.pp
+++ site.pp
@@ -1,10 +1,15 @@
 node default {
-  
-  class { 'couchdb': 
+
+  class { 'couchdb':
     bind => '0.0.0.0',
   }
-  
+
   class { 'elasticsearch':
     bind_host => '0.0.0.0',
+  }
+
+  elasticsearch::plugin { 'couchdb-river':
+    username  => 'elasticsearch',
+    repo_name => 'elasticsearch-river-couchdb',
   }
 }

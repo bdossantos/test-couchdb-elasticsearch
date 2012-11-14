0f50084da2ae2cbacd0f046fb2ca85e0e679f091
Benjamin Dos Santos <benj.ds@gmail.com>
2012-11-14 21:33:37 +0100

    Classes relationship

--- site.pp
+++ site.pp
@@ -6,11 +6,13 @@ node default {
 
   class { 'elasticsearch':
     bind_host => '0.0.0.0',
+    require   => Class['couchdb'],
   }
 
   elasticsearch::plugin { 'couchdb-river':
     username  => 'elasticsearch',
     repo_name => 'elasticsearch-river-couchdb',
     version   => '1.1.0',
+    require   => Class['elasticsearch'],
   }
 }
